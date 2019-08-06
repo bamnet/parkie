1ebb687716ebf150532299aaa472d9b525b6b9a8
Brian M <bmichalski@gmail.com>
2019-08-05 23:54:28 -0400

    Fix e2e tests.

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,9 +8,9 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display app name', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to parkie!');
+    expect(page.getTitleText()).toEqual('Parkie');
   });
 
   afterEach(async () => {
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
   }
 
   getTitleText() {
-    return element(by.css('app-root h1')).getText() as Promise<string>;
+    return element.all(by.css('app-root span')).first().getText() as Promise<string>;
   }
 }
